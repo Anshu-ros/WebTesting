2e00d6a535ccb1c31fccc1180dffacea84c58cf1
Anshu <anshuyadavros@gmail.com>
2017-12-03 17:57:11 +0530

    Module to find roots of quadratic equation.

---
+++ root.c
@@ -0,0 +1,11 @@
+int sf1(int a,int b,int c)
+{
+  int root1=((-b)+sqrt(b*b-(4*a*c)))/(2*a);
+  return root1;
+}
+
+int sf2(int a,int b,intc)
+{
+  int root2=((-b)-sqrt(b*b-(4*a*c)))/(2*a);
+  return root2;
+}
