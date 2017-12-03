dc7fce31efe96fa2b6cca1e4700afbaecd2e007e
Anshu <anshuyadavros@gmail.com>
2017-12-03 18:58:19 +0530

    main file modified with two module.

--- SolofQuad.c
+++ SolofQuad.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 #include "Discriminant.c"
+#include "root.c"
 
 int main ()
 {
@@ -9,5 +11,8 @@ int main ()
   int D;
   D= D1(a,b,c);
   printf("Discriminant of The euation is %d.",D);
+  int rt1=sf1(a,b,c);
+  int rt2=sf2(a,b,c);
+  printf("\nroots of the equation are %d %d.",rt1,rt2);
   return 0;
 }
