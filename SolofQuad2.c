ee7e15b2a7c6f4408c352aea31eb2e80cb580f8b
Anshu <anshuyadavros@gmail.com>
2017-12-04 09:17:50 +0530

    Main file with integrated module.

---
+++ SolofQuad2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include "Discriminant.c"
+#include "insolution.c"
+#include "inroot.c"
+
+int main ()
+{
+  int a,b,c;
+  printf("enter the coefficients of quadratic equation.");
+  scanf("%d%d%d",&a,&b,&c);
+  int D,pr1,pr2,rt1,rt2;
+  D=D1(a,b,c);
+  pr1=ins1(b,D);
+  pr2=ins2(b,D);
+  rt1=sol1(a,pr1);
+  rt2=sol1(a,pr2);
+  printf("\nroots of the equation are %d %d.",rt1,rt2);
+  return 0;
+}
