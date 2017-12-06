ae92b56ec209f5204015e23b93e2a49ec247ad29
Anshu <anshuyadavros@gmail.com>
2017-12-06 17:42:03 +0530

    proceeding towards solutions.

--- solution.c
+++ solution.c
@@ -1,11 +1,11 @@
-int s1(int a,int b,int c)
+float s1(float a,float b,float c)
 {
-  int pr1= (-b)+sqrt(b*b-(4*a*c));
+  float pr1= (-b)+sqrt(b*b-(4*a*c));
   return pr1;
 }
 
-int s2(int a,int b,int c)
+float s2(float a,float b,float c)
 {
-  int pr2=(-b)-sqrt(b*b-(4*a*c));
+  float pr2=(-b)-sqrt(b*b-(4*a*c));
   return pr2;
 }
